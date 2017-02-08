ddb7c323a4b1fc9eb38aabfe5fa9a56ee8bc032f
katherine tuter <katherinetuter@gmail.com>
2017-02-08 10:29:07 -0600

    worked on if loop

--- alpha-stacker.js
+++ alpha-stacker.js
@@ -1,16 +1,19 @@
 // Create an array that contains the letters of the alphabet
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var row = " ";
 
 // The stackLetter function should accept the array as the sole argument
 
 
-function stackLetter(alphabet) {
-   	var i;
-    for (var i = 0; i <= alphabet.length; i + 1) {
-    alphabet.push({num:i});
-	console.log(alphabet);
+
+function stackLetter() {
+	for (var i = 0; i < alphabet.length; i++) { 
+	var split = i / 3;
+	if (split = i) {
+		row += " ";}
+	var stack = row += alphabet[i];
 	};	
-};
+console.log(stack);
+ };
 
-
-
+stackLetter(alphabet);
