71c0cc0ac1c77d894ac26a99e8806fe30be10303
katherine tuter <katherinetuter@gmail.com>
2017-02-08 21:46:06 -0600

    still figuring out loops in function

--- alpha-stacker.js
+++ alpha-stacker.js
@@ -1,19 +1,23 @@
 // Create an array that contains the letters of the alphabet
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var row = " ";
+
+
 
 // The stackLetter function should accept the array as the sole argument
 
+function stackLetter(alphabet) {
+	for (var i = 1; i < alphabet[0].length; i++) {
+		console.log(index); //why is chrome not working
+		var row = ""; //empty string to loop through
+	for(var l = 0; l < index; l++) {
+		row += alphabet[0][l]; //show a ab abc..format
+		if (l % 3 == 0) //split a ab abc per 3 letters
+		row += " "; //new tree format
+		}
+	}
+console.log(row);
+};
+	
 
 
-function stackLetter() {
-	for (var i = 0; i < alphabet.length; i++) { 
-	var split = i / 3;
-	if (split = i) {
-		row += " ";}
-	var stack = row += alphabet[i];
-	};	
-console.log(stack);
- };
 
-stackLetter(alphabet);
