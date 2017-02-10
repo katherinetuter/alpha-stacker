ff288a8cc2552814f4db9e60f2dad071382af3c6
katherine tuter <katherinetuter@gmail.com>
2017-02-09 19:51:12 -0600

    finished exercise

--- alpha-stacker.js
+++ alpha-stacker.js
@@ -1,23 +1,23 @@
 // Create an array that contains the letters of the alphabet
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-
-
-// The stackLetter function should accept the array as the sole argument
-
-function stackLetter(alphabet) {
-	for (var i = 1; i < alphabet[0].length; i++) {
-		console.log(index); //why is chrome not working
-		var row = ""; //empty string to loop through
-	for(var l = 0; l < index; l++) {
-		row += alphabet[0][l]; //show a ab abc..format
-		if (l % 3 == 0) //split a ab abc per 3 letters
-		row += " "; //new tree format
+//The stackLetter function should accept the array as the sole argument
+function stackLetter() {
+	for (var i = 0; i < alphabet.length; i++) {
+		console.log(alphabet.slice(0,i));
+		var row = " ";
 		}
-	}
-console.log(row);
-};
-	
+	for (var l = 0; l < alphabet.length; l++) {
+	 	row += alphabet[l];
+		if (l % 3 == 0 && l != 0) {	
+		row += " ";
+		}
+	console.log(row);
+	}		
+}
+
+stackLetter(alphabet);
+
 
 
 
