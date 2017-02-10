e452a8d6b9ff658d27976a78c9a6610fb38480fc
katherine tuter <katherinetuter@gmail.com>
2017-02-09 19:53:45 -0600

    commented out for future reference

--- alpha-stacker.js
+++ alpha-stacker.js
@@ -4,19 +4,19 @@ var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n",
 //The stackLetter function should accept the array as the sole argument
 function stackLetter() {
 	for (var i = 0; i < alphabet.length; i++) {
-		console.log(alphabet.slice(0,i));
-		var row = " ";
+		console.log(alphabet.slice(0,i)); //to slice array to show a ab abc..
+		var row = " "; //create empty string to use below
 		}
 	for (var l = 0; l < alphabet.length; l++) {
-	 	row += alphabet[l];
-		if (l % 3 == 0 && l != 0) {	
-		row += " ";
+	 	row += alphabet[l]; //add to row from array to split
+		if (l % 3 == 0 && l != 0) {	//split by 3
+		row += " "; //add empty space after each 3
 		}
-	console.log(row);
+	console.log(row); //finished product
 	}		
 }
 
-stackLetter(alphabet);
+stackLetter(alphabet); //call the function to run it
 
 
 
